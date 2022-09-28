5e80f62739ae0f766a30e57c3e4a084f120a990f
Anna Levchenko <levallevchenko@gmail.com>
2022-09-28 23:38:34 +0300

    init: add project config

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>Kanban Board</h1>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import "../sass/styles.scss";
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));

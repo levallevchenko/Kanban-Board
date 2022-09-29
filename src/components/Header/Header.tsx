44bcf6aae3bd5c8faff1eccea5ff9653597cc4da
Anna Levchenko <levallevchenko@gmail.com>
2022-09-29 14:31:50 +0300

    refactor: add folders for project components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
+import Profile from '../Profile/Profile';
+
+export default function Header() {
+	return (
+		<>
+			<Logo />
+			<Navigation />
+			<Profile />
+		</>
+	);
+}
